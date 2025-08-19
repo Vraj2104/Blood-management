1cf34565d4fa0a811242afeadcf74084ce697e20
Vraj Patel <vrajlic2104@gmail.com>
2025-08-19 19:22:32 +0530

    chore: update frontend files

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://blood-management-backend-3hku.onrender.com';
 
 export async function getBloodStock() {
   const res = await fetch(`${API_BASE_URL}/blood-stock`);
